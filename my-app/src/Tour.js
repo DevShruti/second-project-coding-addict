557148abc8c90271bbf7e7030c91b2cec8f14a1e
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-04-08 10:38:11 -0500

    final

--- my-app/src/Tour.js
+++ my-app/src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({ id, image, info, name, price }) => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
     const [readMore, setReadMore] = useState(false);
     return (
         <article className="single-tour">
@@ -14,7 +14,7 @@ const Tour = ({ id, image, info, name, price }) => {
             <button onClick={() => setReadMore(!readMore)}>
             {readMore ? 'show less' : '  read more'}
           </button></p>
-            <button className='delete-btn'>not interested</button>
+            <button className='delete-btn' onClick={() => removeTour(id)}>not interested</button>
             </footer>
             </article>
     )
